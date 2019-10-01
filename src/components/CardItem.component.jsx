2abe29f0bc7965aa6aa26fec261afecdf15f5152
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-10-01 11:20:58 -0700

    Addstyles to home and card items

---
+++ src/components/CardItem.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./CardItem.styles.scss";
+
+const CardItem = () => {
+  return <div className='card-item'>Card Item -1</div>;
+};
+
+export default CardItem;
--- src/pages/home/Home.component.jsx
+++ src/pages/home/Home.component.jsx
@@ -1,22 +1,43 @@
 import React, { Component } from "react";
 import "./Home.styles.scss";
+import CardItem from "../../components/CardItem.component";
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cardItems: [
+        {
+          category: "hat",
+          image_url: "https://via.placeholder.com/150/92c952"
+        },
+        {
+          category: "shoes",
+          image_url: "https://via.placeholder.com/150/92c952"
+        },
+        {
+          category: "accessary",
+          image_url: "https://via.placeholder.com/150/92c952"
+        },
+        {
+          category: "women",
+          image_url: "https://via.placeholder.com/150/92c952"
+        },
+        {
+          category: "men",
+          image_url: "https://via.placeholder.com/150/92c952"
+        }
+      ]
+    };
+  }
   render() {
     return (
-      <div className='home-container'>
-        <div className='card-item'>
-          <div>hoho</div>
-        </div>
-        <div className='card-item'>
-          <div>ajuma</div>
-        </div>
-        <div className='card-item'>
-          <div>kiki</div>
-        </div>
-        <div className='card-item'>
-          <div>chicken</div>
-        </div>
+      <div className='home'>
+        <CardItem className='card-item' />
+        <CardItem className='card-item' />
+        <CardItem className='card-item' />
+        <CardItem className='card-item' />
+        <CardItem className='card-item' />
       </div>
     );
   }
