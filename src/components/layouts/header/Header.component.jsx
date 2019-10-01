90a374ec84bc3b75466b9cb3430632fb21b0c4a2
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-10-01 09:15:25 -0700

    Add basic layout to the app

---
+++ src/components/layouts/header/Header.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Header.styles.scss";
+
+const Header = () => {
+  return <div className='Header'>HEADER</div>;
+};
+
+export default Header;
---
+++ src/pages/home/Home.component.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./Home.styles.scss";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className='home-container'>
+        <div className='card-item'>
+          <div>hoho</div>
+        </div>
+        <div className='card-item'>
+          <div>ajuma</div>
+        </div>
+        <div className='card-item'>
+          <div>kiki</div>
+        </div>
+        <div className='card-item'>
+          <div>chicken</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
