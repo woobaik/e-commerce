0afe706355ec49a587e55de38e1a7a51837480be
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-10-03 22:33:42 -0700

    Implement base database

---
+++ src/shop.data.js
@@ -0,0 +1,129 @@
+const data = {
+  collections: [
+    {
+      id: 1,
+      title: "Hats",
+      routeName: "Hats",
+      items: [
+        {
+          id: 1,
+          name: "Tom Brown Cap",
+          price: 25.99
+        },
+        {
+          id: 2,
+          name: "ASSC Cap",
+          price: 11.99
+        },
+        {
+          id: 3,
+          name: "Supreme Camo Cap",
+          price: 25.99
+        },
+        {
+          id: 4,
+          name: "League of Legends Event Cap",
+          price: 11.99
+        },
+        {
+          id: 5,
+          name: "Jordan Spring Cap",
+          price: 30.99
+        },
+        {
+          id: 6,
+          name: "Chicken head Cap",
+          price: 33.99
+        }
+      ]
+    },
+    {
+      id: 2,
+      title: "Shoes",
+      routeName: "Shoes",
+      items: [
+        {
+          id: 1,
+          name: "Adidas Yeezy Citrin",
+          price: 249.99
+        },
+        {
+          id: 2,
+          name: "Nike SCAI",
+          price: 129.99
+        },
+        {
+          id: 3,
+          name: "Supreme X NIKE Dunk Low",
+          price: 149.99
+        },
+        {
+          id: 4,
+          name: "KITH X NIKE",
+          price: 199.99
+        },
+        {
+          id: 5,
+          name: "Adidas NMD",
+          price: 249.99
+        }
+      ]
+    },
+    {
+      id: 3,
+      title: "Accessary",
+      routeName: "accessary",
+      items: [
+        {
+          id: 1,
+          name: "Supreme MiniCar",
+          price: 33.99
+        },
+        {
+          id: 2,
+          name: "Supreme Surf Board",
+          price: 129.99
+        },
+        {
+          id: 3,
+          name: "Happy Hacking Keyboard for Mac",
+          price: 79.99
+        },
+        {
+          id: 4,
+          name: "Amazon Alexa Bluetooth Sticker",
+          price: 59.99
+        },
+        {
+          id: 5,
+          name: "Supreme Key Ring",
+          price: 34.99
+        }
+      ]
+    },
+    {
+      id: "4",
+      title: "Women",
+      routeName: "women",
+      items: [
+        {
+          id: "1",
+          name: "VIVIAN WEST WOOD TEE",
+          price: 89.99
+        },
+        {
+          id: "2",
+          name: "Save the tree",
+          price: 19.99
+        },
+        {
+          id: "3",
+          name: "Chicken man",
+          prince: 20.99
+        }
+      ]
+    }
+  ]
+}
+
+export default data
