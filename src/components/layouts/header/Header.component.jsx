cbb6efef3a44ef0ea596b5e8fbffe1d61ee6e9d4
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-10-04 18:54:49 -0700

    Add mock-up design for header

--- src/components/layouts/header/Header.component.jsx
+++ src/components/layouts/header/Header.component.jsx
@@ -1,8 +1,17 @@
-import React from "react";
-import "./Header.styles.scss";
+import React from "react"
+import "./Header.styles.scss"
 
 const Header = () => {
-  return <div className='Header'>HEADER</div>;
-};
+  return (
+    <div className='Header'>
+      <div className='header-logo'>LOGO</div>
+      <div className='header-navigations'>
+        <div>navigation</div>
+        <div>navigation</div>
+        <div>navigation</div>
+      </div>
+    </div>
+  )
+}
 
-export default Header;
+export default Header
