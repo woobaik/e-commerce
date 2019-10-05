f2c7e9e37133bd3217eebef45951dff9b455ade8
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-10-04 18:32:04 -0700

    Change image styling for shop

--- src/components/shop/ShopItem.component.jsx
+++ src/components/shop/ShopItem.component.jsx
@@ -2,7 +2,20 @@ import React from "react"
 
 import "./ShopItem.styles.scss"
 function ShopItem({ card }) {
-  return <div className='shop-item'>{card.name}</div>
+  console.log(card)
+  return (
+    <div className='shop-item'>
+      <div
+        className='shop-item-image'
+        style={{ backgroundImage: `url(${card.imageUrl})` }}
+      ></div>
+      <div className='shop-item-detail'>
+        <div>{card.name}</div>
+        <div>${card.price}</div>
+      </div>
+      <span></span>
+    </div>
+  )
 }
 
 export default ShopItem
