7af11534cf383316f51bbaa76d769e46e45f815a
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-10-06 12:57:01 -0700

    Add more styles and changed state imageURL

--- src/components/layouts/footer/Footer.component.jsx
+++ src/components/layouts/footer/Footer.component.jsx
@@ -1,10 +1,30 @@
 import React from "react"
 import "./Footer.styles.scss"
+import CopyrightIcon from "@material-ui/icons/Copyright"
+import Icon from "@material-ui/core/Icon"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import { faCopyright } from "@fortawesome/free-regular-svg-icons"
 
 const Footer = () => {
   return (
     <div className='footer'>
-      <div></div>
+      <div>
+        <FontAwesomeIcon icon={faCopyright} />
+        Joungwoo Baik All Rights Reserved
+      </div>
+      <div>
+        <a
+          className='footer-link'
+          href='https://www.github.com/woobaik'
+          target='_blank'
+        >
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a className='footer-link' href='https://www.linkedin.com/in/baik/.'>
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+      </div>
     </div>
   )
 }
--- src/pages/home/Home.component.jsx
+++ src/pages/home/Home.component.jsx
@@ -10,28 +10,33 @@ class Home extends Component {
       cardItems: [
         {
           category: "hat",
-          image_url: "https://www.fillmurray.com/g/200/300",
+          image_url:
+            "https://cdn.pixabay.com/photo/2016/12/11/00/39/cat-1898503_960_720.jpg",
           id: 1
         },
         {
           category: "shoes",
-          image_url: "https://www.fillmurray.com/g/200/300",
+          image_url:
+            "https://hautecature.files.wordpress.com/2013/01/catshoes.jpg",
           id: 2
         },
         {
           category: "accessary",
-          image_url: "https://www.fillmurray.com/g/200/300",
+          image_url:
+            "https://www.dhresource.com/0x0s/f2-albu-g7-M00-D9-8B-rBVaSlvtMAuAUJ2gAAN9Sx6CM7Q014.jpg/christmas-elk-headband-for-cats-hair-decorations.jpg",
           id: 3
         },
         {
           category: "women",
-          image_url: "https://www.fillmurray.com/g/200/300",
+          image_url:
+            "https://i.dailymail.co.uk/i/pix/2012/07/03/article-2168089-13E71191000005DC-707_468x543.jpg",
           id: 4,
           classProp: "image-large"
         },
         {
           category: "men",
-          image_url: "https://www.fillmurray.com/g/200/300",
+          image_url:
+            "https://i.pinimg.com/originals/e4/a7/b9/e4a7b9965c6bf93aa11f8d888f7ad92a.jpg",
           id: 5,
           classProp: "image-large"
         }
